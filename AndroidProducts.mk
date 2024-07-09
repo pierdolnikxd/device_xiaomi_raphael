08867c250afeb994301222daa9d4d095a467d663
pawelik001 <pawelik001@gmail.com>
2024-07-09 14:54:06 +0200

    raphael: Bring up Infinity X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/infinity_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_raphael-user \
-    lineage_raphael-userdebug \
-    lineage_raphael-eng
+    infinity_raphael-user \
+    infinity_raphael-userdebug \
+    infinity_raphael-eng
